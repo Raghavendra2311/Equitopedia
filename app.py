75031fda2d09949e4e42456f0d4ac45fc36e5c83
Raghavendra Kharosekar <55637197+Raghavendra2311@users.noreply.github.com>
2023-12-04 13:52:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ if option =='Quarterly Analysis':
     st.write(yf.Ticker(ticker).quarterly_financials)
 
 if option == 'Live Market Price':
-    st.write(pd.DataFrame(yf.Ticker(ticker).get_info()))
+    st.write(yf.Ticker(ticker).get_info())
     now = datetime.datetime.now()
     time1 = now.strftime("%H:%M:%S")
     tickers = yf.Ticker(ticker)
